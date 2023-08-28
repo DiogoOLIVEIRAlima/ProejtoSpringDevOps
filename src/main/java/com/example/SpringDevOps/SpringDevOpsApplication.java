58b135e9145662b387038a4bf1e7a261aa7cfb81
Diogo <limaa.diogi@gmail.com>
2023-08-28 18:15:32 -0300

    projetinho

---
+++ src/main/java/com/example/SpringDevOps/SpringDevOpsApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringDevOps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDevOpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDevOpsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringDevOps/SpringDevOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringDevOps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDevOpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
